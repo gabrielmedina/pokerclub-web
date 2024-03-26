9fe26d66306dff8762767a666ace81838ae68c9f
Gabriel Medina <gmedina.santos@gmail.com>
2024-03-26 10:34:53 -0400

    feat: getTournaments

--- src/app/tournaments/page.tsx
+++ src/app/tournaments/page.tsx
@@ -1,11 +1,31 @@
 import LayoutDefault from '@/components/layouts/default'
+import { PaginationType } from '@/types/Pagination'
+import { TournamentType } from '@/types/Tournament'
 
-export default function Tournaments() {
+export default async function Tournaments() {
+  const tournaments = await getTournaments()
+
   return (
     <main>
       <LayoutDefault>
         <h1>Tournaments</h1>
+
+        <ul>
+          {tournaments.content.map((tournaments) => (
+            <li key={tournaments.id}>{tournaments.title}</li>
+          ))}
+        </ul>
       </LayoutDefault>
     </main>
   )
+}
+
+async function getTournaments(): Promise<PaginationType<TournamentType>> {
+  const response = await fetch(`${process.env.API_URL}/tournaments`)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch players data')
+  }
+
+  return response.json()
 }
---
+++ src/types/Tournament.ts
@@ -0,0 +1,7 @@
+export type TournamentType = {
+  id: string
+  title: string
+  buyin: number
+  createdAt: string
+  finishedAt: string
+}
